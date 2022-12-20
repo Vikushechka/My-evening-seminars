499914090f0f0ad8a8072354e7999c78dd500dba
«Viktoriya <catwomen.07@mail.ru>
2022-12-20 16:19:07 +0300

    Второе дз

---
+++ Homeworks/Homeworks2/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// int SecondNumber(int num)
+// {
+//     int dec = num / 10;
+//     int ed = dec % 10;
+//     return ed;
+// }
+
+// Console.WriteLine("Введите трехзначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SecondNumber(num));
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// int ThirdNumber (int num)
+// {
+//     if(num >= 100 && num < 1000)
+//     {
+//         num = num % 10;
+//         return num;
+//     }
+//     else
+//     {
+//         if (num >=1000)
+//         {
+//             while(num >=1000)
+//             {
+//                 num =num / 10;
+//             }
+//             num = num % 10;
+//             return num;
+//         }
+//         else
+//         {
+//             Console.WriteLine($"{num} -> Третьей цифры нет");
+//             return num;
+//         }
+//     }
+// }
+
+// Console.WriteLine ("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine(ThirdNumber(num));
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// int Week (int num)
+// {
+//     if(num > 5)
+//     {
+//         Console.WriteLine($"{num} -> да");
+//         return num;
+//     }
+//     else
+//     {  
+//         Console.WriteLine($"{num} -> нет");
+//         return num;
+//     }
+// }
+
+// Console.WriteLine("Введите цифру от 1 до 7");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Week(num));
